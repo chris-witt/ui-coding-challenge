83a898b6e08fdff6e8d65933b2710f3bc66a38c3
Chris Witt <chris@toadstool.app>
2018-09-22 11:56:16 -0400

    testing with multiple months, looks good but need to figure out empty days

--- src/app/calendar-display/calendar-display.component.ts
+++ src/app/calendar-display/calendar-display.component.ts
@@ -22,7 +22,27 @@ export class CalendarDisplayComponent implements OnInit {
                  new Date('9/26/2018'),
                  new Date('9/27/2018'),
                  new Date('9/28/2018'),
-                 new Date('9/29/2018'), ] }
+                 new Date('9/29/2018'), ] },
+            { days: [ new Date('9/30/2018'), ] }
+          ]
+        },
+        {
+          month: 'October',
+          year: 2018,
+          weeks: [
+            { days: [ new Date('10/1/2018'),
+                 new Date('10/2/2018'),
+                 new Date('10/3/2018'),
+                 new Date('10/4/2018'),
+                 new Date('10/5/2018'),
+                 new Date('10/6/2018'), ] },
+            { days: [ new Date('10/7/2018'),
+                 new Date('10/8/2018'),
+                 new Date('10/9/2018'),
+                 new Date('10/10/2018'),
+                 new Date('10/11/2018'),
+                 new Date('10/12/2018'),
+                 new Date('10/13/2018'), ] },
           ]
         }
       ];
